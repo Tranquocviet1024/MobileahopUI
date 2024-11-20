43c43a796fb912b6d48c8caab02439f48d471f45
Hanaviet <tranquocvieta4k51nl@gmail.com>
2024-11-20 13:11:46 +0700

    final v2

--- obj/Debug/net8.0-windows/MobileShopUI.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/MobileShopUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobileShopUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e1cc8f30dc4fa8e61fe24e5963a864c824f7a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6075b7c4c49a47ee27635bfef23475151a8914d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobileShopUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobileShopUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
